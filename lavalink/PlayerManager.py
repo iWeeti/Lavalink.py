e539f4e53efa99f64b6bbb262c347e1051452dbd
iWeeti <40580113+iWeeti@users.noreply.github.com>
2018-10-05 18:57:35 +0300

    Update PlayerManager.py

--- lavalink/PlayerManager.py
+++ lavalink/PlayerManager.py
@@ -133,7 +133,6 @@ class DefaultPlayer(BasePlayer):
 
     async def play_at(self, index: int):
         """ Play the queue from a specific point. Disregards tracks before the index. """
-        self.queue = self.queue[min(index, len(self.queue) - 1):len(self.queue)]
         await self.play(ignore_shuffle=True)
 
     async def play_previous(self):
